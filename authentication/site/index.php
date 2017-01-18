75424dcc586858daeb50fc3dd897ec23ee292d3b
Marcel Großmann <marcel.grossmann@uni-bamberg.de>
2017-01-18 11:46:49 +0100

    First Part

---
+++ authentication/site/index.php
@@ -0,0 +1,19 @@
+ <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+    "http://www.w3.org/TR/html4/loose.dtd">
+ <html>
+   <link rel="icon" href="/Globe.ico" type="image/x-icon">
+   <link rel="shortcut icon" href="/Globe.ico" type=”image/x-icon” />
+   <head>
+     <title>Hello-World-Example</title>
+   </head>
+   <body>
+    	<?php
+       	echo "Hello World!";
+     	?>
+   	<p>
+	   <img src="Globe.png" alt="Globe"/>
+   	</p>
+	   <a href="/safe/safe.html">Secret!?</a><br>
+	   <a href="/safe2/safer.html">More secure!</a><br>
+	   <a href="/ssl/strictlyConfidential.html">Strictly confidential!</a><br>
+ </html>
