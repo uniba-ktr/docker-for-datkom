f56addf5b4ae0ed90a4314ad71ebcbae4760fac1
Marcel Großmann <marcel.grossmann@uni-bamberg.de>
2017-01-20 14:40:44 +0100

    HTTP2 Server Push

--- apache/site/ssl/js/load.js
+++ apache/site/ssl/js/load.js
@@ -1,8 +1,10 @@
 function loadImages() {
   var slide=$("#slide")
-  for (i=1; i<=54; i++){
+
+  for (i=1; i<=5; i++){
     var img=document.createElement("img");
-    img.src="images/DM-" + i + ".jpg"
+    img.src="/ssl/images/DM-" + i + ".jpg"
+    img.setAttribute("rel", "preload");
     slide.append(img);
   }
   $('.fotorama').fotorama();
